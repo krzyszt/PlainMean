28299b38bfbd7107714aa672dc50038c0bba208d
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-08-19 17:29:39 +0100

    test grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,43 @@
-module.exports = function(grunt){
-	
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON("package.json"),
-		
-		/**
-         * JSHint
-         *
-         * Validate the source code files to ensure they follow our coding convention and
-         * don"t contain any common errors.
-         */
-		jshint: {
-			
-		}
-	});
-	
-	//Load the plugin.
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	
-	//Dafault task(s).
-	grunt.registerTask('default',['jshint']);
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    
+    jshint: {
+      files: ['Gruntfile.js', 'public/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint','uglify']);
+
 };
