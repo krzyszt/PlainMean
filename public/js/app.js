ed2d431798591007948998876904aa9c91ea4468
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-08-20 12:14:37 +0100

    added controllers

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 angular.module('plainMean',[])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider.
-            when('/list', { templateUrl: 'partials/list.html', controller: MainCtrl}).
+            when('/list', { templateUrl: 'partials/list.html', controller: ListCtrl}).
             when('/item/:itemId', { templateUrl: 'partials/item.html', controller: ItemCtrl}).
             when('/new', { templateUrl: 'partials/new_item.html', controller: NewItemCtrl}).
             otherwise.redirectTo('/list');
---
+++ public/js/controllers.js
@@ -0,0 +1,14 @@
+/**
+ * Controllers
+ */
+
+angular.module('plainMean.controllers',[])
+ .controller('ListCtrl', ['$scope',function($scope){
+     
+ }])
+ .controller('ItemCtrl', ['$scope',function($scope){
+     
+ }])
+ .controller('NewItemCtrl', ['$scope',function($scope){
+     
+ }])
