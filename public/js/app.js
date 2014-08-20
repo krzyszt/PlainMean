30661790c67cbe04a185852ad7772799201caf81
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-08-20 15:14:16 +0100

    list controller & routing modified

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
-angular.module('plainMean',[])
+angular.module('plainMean',[
+    'plainMean.controllers'
+    ])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider.
-            when('/list', { templateUrl: 'partials/list.html', controller: ListCtrl}).
-            when('/item/:itemId', { templateUrl: 'partials/item.html', controller: ItemCtrl}).
-            when('/new', { templateUrl: 'partials/new_item.html', controller: NewItemCtrl}).
-            otherwise.redirectTo('/list');
+            when('/list', { templateUrl: 'partials/list.html', controller: 'ListCtrl' }).
+            when('/item/:customerId', { templateUrl: 'partials/item.html', controller: 'ItemCtrl' }).
+            when('/new', { templateUrl: 'partials/new.html', controller: 'NewCtrl' }).
+            otherwise({ redirectTo: '/list' });
     }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,12 +3,14 @@
  */
 
 angular.module('plainMean.controllers',[])
- .controller('ListCtrl', ['$scope',function($scope){
-     
- }])
- .controller('ItemCtrl', ['$scope',function($scope){
-     
- }])
- .controller('NewItemCtrl', ['$scope',function($scope){
-     
- }])
+   .controller('ListCtrl',['$scope', function($scope){
+     $scope.customers = [];
+     $scope.customers.push({ name: 'Dummy Company 1', city: 'Bristol'});
+     $scope.customers.push({ name: 'Dummy Company 2', city: 'London'});
+     $scope.customers.push({ name: 'Dummy Company 3', city: 'Croydon'});
+     $scope.customers.push({ name: 'Dummy Company 4', city: 'Manchester'});
+     $scope.customers.push({ name: 'Dummy Company 5', city: 'Bristol'});
+     $scope.customers.push({ name: 'Dummy Company 6', city: 'London'});
+     $scope.customers.push({ name: 'Dummy Company 7', city: 'Croydon'});
+     $scope.customers.push({ name: 'Dummy Company 8', city: 'Manchester'});
+   }]);
