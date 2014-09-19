a24b2c34086898360d7d5732096a440056d2c2aa
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-09-19 08:40:22 +0100

    new contact route & controller frontend

--- public/crm/contact/contact.ctrl.js
+++ public/crm/contact/contact.ctrl.js
@@ -4,4 +4,16 @@ angular.module("plainMean")
    }])
     .controller('ContactPopulateCtrl', ['$scope','$route','ContactPopulate',function($scope, $route, ContactPopulate){
        $scope.contacts = ContactPopulate.query();
+   }])
+   .controller('ContactNewCtrl',['$scope', 'Contact', function($scope, Contact){
+       $scope.createContact = function(){
+           Contact.save($scope.contact, function(ref){
+               $scope.contact._id = ref._id;
+               $scope.contact = {
+                       firstName: '',
+                       lastName: '',
+                       email: ''
+               };
+           });
+       };
    }]);
