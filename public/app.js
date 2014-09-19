d0feed5a3efd74611b2b626c5ac85e0bde585d00
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-09-19 07:58:16 +0100

    contact route & controller

--- public/app.js
+++ public/app.js
@@ -10,5 +10,6 @@ angular.module('plainMean',[
             when('/customer/:id', { templateUrl: 'admin/item.tpl.html', controller: 'ItemCtrl' }).
             when('/new', { templateUrl: 'admin/new.tpl.html', controller: 'NewCtrl' }).
             when('/contact', { templateUrl: 'crm/contact/list.tpl.html', controller: 'ContactListCtrl' }).
+            when('/contact/populate', { templateUrl: 'crm/contact/populate.tpl.html', controller: 'ContactPopulateCtrl' }).
             otherwise({ redirectTo: '/' });
     }]);
--- public/crm/contact/contact.ctrl.js
+++ public/crm/contact/contact.ctrl.js
@@ -1,4 +1,7 @@
-angular.module("plainMean"). 
-    controller("ContactListCtrl", ["$scope", function($scope){
-        $scope.contactFirst = "John Doe";
-}]);
+angular.module("plainMean")
+    .controller('ContactListCtrl', ['$scope','$route','Contact',function($scope, $route, Contact){
+       $scope.contacts = Contact.query();
+   }])
+    .controller('ContactPopulateCtrl', ['$scope','$route','ContactPopulate',function($scope, $route, ContactPopulate){
+       $scope.contacts = ContactPopulate.query();
+   }]);
