c07be265288b3ed4411cd36cf92d9b5bbcc3347d
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-09-19 07:55:21 +0100

    contact  service

--- public/common/services.js
+++ public/common/services.js
@@ -3,4 +3,14 @@ angular.module('customerServices', ['ngResource'])
     return $resource('/customer/:customerId',{}, {
         query: { method: 'GET', params: {customerId: ''}, isArray: true} 
     });
+ }])
+.factory('Contact',[ '$resource', function($resource){
+    return $resource('/contact/:contactId',{}, {
+        query: { method: 'GET', params: {contactId: ''}, isArray: true} 
+    });
+ }])
+.factory('ContactPopulate',[ '$resource', function($resource){
+    return $resource('/contact/populate',{}, {
+        query: { method: 'GET', params: {contactId: ''}, isArray: true} 
+    });
  }]);
