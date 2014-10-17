95c3f4cb8791f63dad3d48a62c03d64807e6e395
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-10-17 10:35:07 +0100

    controller modified

--- public/crm/customer/customer.ctrl.js
+++ public/crm/customer/customer.ctrl.js
@@ -1,27 +1,51 @@
 angular.module('plainMean.customer', [])
-   .controller('CustomerListCtrl', ['$scope','$route','Customer',function($scope, $route, Customer){
-       $scope.customers = Customer.query();
-   }])
-   .controller('CustomerItemCtrl', ['$scope','$routeParams', 'Customer', function($scope, $routeParams, Customer){
+   .controller('CustomerListCtrl', ['$scope', '$route', 'Customer', function($scope, $route, Customer) {
+         $scope.customers = Customer.query();
       
-         $scope.pageTitle = "Create Customer";
+         $scope.selectCustomer = function(customer){
+           $scope.selectedCustomer = customer;
+         };
          
-         $scope.save = function(){
-            if( $routeParams.id == 0 ) {
-               Customer.save($scope.customer, function(ref){
+         $scope.isSelected = function(customer) {
+            return $scope.selectedCustomer === customer;
+         };
+      
+      }])
+   .controller('CustomerItemCtrl', ['$scope', '$routeParams', 'Customer', 'Contact', function($scope, $routeParams, Customer, Contact) {
+
+         $scope.pageTitle = "Create Customer";
+         $scope.contacts = Contact.query();
+
+         if ($routeParams.id != 0) {
+            $scope.customer = Customer.get({customerId: $routeParams.id});
+            $scope.pageTitle = "Update Customer";
+         } else {
+            $scope.customer = {
+               name: '',
+               city: '',
+               type: '',
+               status: '',
+               description: ''
+            };
+         };
+         
+
+         $scope.save = function() {
+            if ($routeParams.id == 0) {
+               Customer.save($scope.customer, function(ref) {
                   $scope.customer._id = ref._id;
                   $scope.customer = {
-                       name: '',
-                       city: ''
+                     name: '',
+                     city: '',
+                     type: '',
+                     status: '',
+                     description: ''
                   };
                });
             } else {
                console.log("Update to implement");
             }
          };
-      
-      if( $routeParams.id != 0 ) {
-         $scope.customer = Customer.get({ customerId: $routeParams.id });
-         $scope.pageTitle = "Update Customer";
-      }
-   }]);
+
+
+      }]);
